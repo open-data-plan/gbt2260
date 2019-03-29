0c6de9977c5e391b29a7d562d771dc3b9d3ba182
kagawagao <kingsongao1221@gmail.com>
2019-03-29 11:19:23 +0800

    fix: add try catch

--- scripts/crawler.js
+++ scripts/crawler.js
@@ -5,37 +5,41 @@ const signale = require('signale')
 
 const start = async () => {
   signale.start('Start crawl data')
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(
-    'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902061009.html'
-  )
-  const data = await page.evaluate(() => {
-    let temp = {}
-    const tds = document.querySelectorAll('.xl7016597')
-    let code = ''
-    Array.from(tds).forEach(td => {
-      const text = td.innerText.trim()
-      if (/\d+/.test(text)) {
-        code = text
-      } else if (!temp[code]) {
-        temp[code] = text
-      }
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(
+      'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902061009.html'
+    )
+    const data = await page.evaluate(() => {
+      let temp = {}
+      const tds = document.querySelectorAll('.xl7016597')
+      let code = ''
+      Array.from(tds).forEach(td => {
+        const text = td.innerText.trim()
+        if (/\d+/.test(text)) {
+          code = text
+        } else if (!temp[code]) {
+          temp[code] = text
+        }
+      })
+      return temp
     })
-    return temp
-  })
 
-  signale.complete('Crawl complete successfully')
-  signale.start('Save File')
-  await promisify(fs.writeFile)(
-    'data/201901.json',
-    JSON.stringify(data, null, 2),
-    {
-      encode: 'utf8'
-    }
-  )
-  await browser.close()
-  signale.success('Success')
+    signale.complete('Crawl complete successfully')
+    signale.start('Save File')
+    await promisify(fs.writeFile)(
+      'data/201901.json',
+      JSON.stringify(data, null, 2),
+      {
+        encode: 'utf8'
+      }
+    )
+    await browser.close()
+    signale.success('Success')
+  } catch (err) {
+    signale.error('Error...')
+  }
 }
 
 start()

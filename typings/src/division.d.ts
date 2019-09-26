d50e514bec86a01487b799a0dc2829d13285e429
kagawagao <kingsongao1221@gmail.com>
2019-09-26 16:14:22 +0800

    fix: fix type definitions

---
+++ typings/src/division.d.ts
@@ -0,0 +1,15 @@
+export default class Division {
+    code: string;
+    name: string;
+    revision: string;
+    province?: Division;
+    prefecture?: Division;
+    constructor(code: string, name: string, revision: string);
+    toString: () => string;
+    valueOf: () => string;
+    toJSON: () => {
+        name: string;
+        code: string;
+    };
+    inspect: () => string;
+}

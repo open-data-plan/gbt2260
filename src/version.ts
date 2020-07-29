5d1cf4fafd0c2e3c619fb3c680f89b00e340261c
kagawagao <kingsongao1221@gmail.com>
2020-07-29 11:20:06 +0800

    feat: update revision

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-import data202003 from './data/202003.json'
+import data202005 from './data/202005.json'
 
-export const revision = '202003'
-export const data = data202003
+export const revision = '202005'
+export const data = data202005
